7c5b6767d5c67ea6ef2ab2425b2caff09e4e5bef
Nihar Prajapati <alienfromsadala@gmail.com>
2026-08-20 12:24:21 +0530

    Use 4*4 matrix

--- DAY-04/05. spiral matrix.py
+++ DAY-04/05. spiral matrix.py
@@ -1,16 +1,23 @@
 def spiralMatrix(l:list,i:int,j:int,m:int,n:int,visited):
     if i==m or j==n or i<0 or j<0 or (i,j) in visited :
         return
+    # Print element
     print(l[i][j])
+    #Append index of element 
     visited.append((i,j))
+    # Move Right
     spiralMatrix(l,i,j+1,m,n,visited)
+    # Move Down
     spiralMatrix(l,i+1,j,m,n,visited)
+    # Move Left
     spiralMatrix(l,i,j-1,m,n,visited)
+    # Move Up
     spiralMatrix(l,i-1,j,m,n,visited)
 
 arr = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [1, 2, 3, 4],
+    [12,13,14,5],
+    [11,16,15,6],
+    [10, 9, 8,7]
 ]
-spiralMatrix(arr,0,0,3,3,[])
+spiralMatrix(arr,0,0,4,4,[])
