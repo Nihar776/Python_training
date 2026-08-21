ebda670f9278dca26c5e8f928068776f83488c4d
Nihar Prajapati <alienfromsadala@gmail.com>
2026-08-21 13:30:15 +0530

    Add Comments

--- DAY-04/04. backtracking in maze.py
+++ DAY-04/04. backtracking in maze.py
@@ -6,9 +6,10 @@ def backtrack(i:int,j:int):
     # Destination Reached
     if i==m-1 and j==n-1:
         return 1
-    #Move to Right
-    down=backtrack(i+1,j)
+    # Move to Right
     right=backtrack(i,j+1)
+    # Move Down
+    down=backtrack(i+1,j)
     return right+down
 
 print(backtrack(0,0))
