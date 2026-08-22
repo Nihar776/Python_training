90ef56a80ee7e5e73532da17655c5dda533dc8dd
Nihar Prajapati <alienfromsadala@gmail.com>
2026-08-22 10:29:01 +0530

    Take input inside init in one line

--- DAY-06/method_in_class.py
+++ DAY-06/method_in_class.py
@@ -1,9 +1,12 @@
 class Employee:
-    def __init__(self,dept:str,role:str,salary:int):
+    def __init__(self):
         # self.name=name.capitalize()
-        self.role=role.capitalize()
-        self.dept=dept.capitalize()
-        self.salary=salary
+        # self.role=role.capitalize()
+        # self.dept=dept.capitalize()
+        # self.salary=salary
+        self.dept,self.role,self.salary=(input("Enter Employee details in this format : Department,Role,Salary: ").title()).split(',')
+        self.salary=int(self.salary)
+        # print(type(self.salary))
 
     def showDetails(self)->dict:
         print(f"Department: {self.dept}")
@@ -12,9 +15,9 @@ class Employee:
         return{'dept':self.dept,'role':self.role,'salary':self.salary}
 
 # askName='Enter Name of the Employee: '
-askDept='Enter Department of the Employee: '
-askRole='Enter Role of the Employee in that Department : '
-askSalary='Enter Salary of the Employee : '
+# askDept='Enter Department of the Employee: '
+# askRole='Enter Role of the Employee in that Department : '
+# askSalary='Enter Salary of the Employee : '
 
-e1=Employee(input(askDept),input(askRole),int(input(askSalary)))
+e1=Employee()
 e1.showDetails()
