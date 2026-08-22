e936ca208ed0b83a11de55bd64503eace28e1da6
Nihar Prajapati <alienfromsadala@gmail.com>
2026-08-22 13:47:59 +0530

    Remove commented lines from previous and Prevent code from executing during import

--- DAY-06/method_in_class.py
+++ DAY-06/method_in_class.py
@@ -1,12 +1,9 @@
 class Employee:
     def __init__(self):
-        # self.name=name.capitalize()
-        # self.role=role.capitalize()
-        # self.dept=dept.capitalize()
-        # self.salary=salary
+
         self.dept,self.role,self.salary=(input("Enter Employee details in this format : Department,Role,Salary: ").title()).split(',')
         self.salary=int(self.salary)
-        # print(type(self.salary))
+
 
     def showDetails(self)->dict:
         print(f"Department: {self.dept}")
@@ -14,10 +11,7 @@ class Employee:
         print(f"Salary : {self.salary}")
         return{'dept':self.dept,'role':self.role,'salary':self.salary}
 
-# askName='Enter Name of the Employee: '
-# askDept='Enter Department of the Employee: '
-# askRole='Enter Role of the Employee in that Department : '
-# askSalary='Enter Salary of the Employee : '
 
-e1=Employee()
-e1.showDetails()
+if __name__ == "__main__":
+    e1 = Employee()
+    e1.showDetails()
